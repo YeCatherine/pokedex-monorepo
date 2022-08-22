e3189ee54d7d5ab6a1586478b850168e669aef2d
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2022-08-23 00:01:32 +0200

    Link to Pokemon

--- packages/pokemon-rest/src/components/02-Organisms/PokemonCard.tsx
+++ packages/pokemon-rest/src/components/02-Organisms/PokemonCard.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-
+import { Box, CardActionArea } from '@mui/material';
+import { Link } from 'react-router-dom';
 import { Spinner } from '@monorepo/components';
 import PokemonImage from './PokemonImage';
 import CaptureButton from '../01-Atoms/Form/CaptureButton/CaptureButton';
@@ -25,26 +25,32 @@ const PokemonCard: React.FC<IPokemonSimpleComponent> = (props) => {
 
   return (
     <Card
-          className="pokemon-card card text-dark text-center">
+      className="pokemon-card card text-dark text-center">
       <CardActionArea>
         <PokemonImage pokemon={pokemon}/>
         <CardContent>
-          <Typography className="pokemon-name" gutterBottom variant="h5"
-                      component="div">
-            {pokemon.name}
-            {pokemon.id &&
-            <><br/><span className="pokemon-id">(#{pokemon.id})</span></>}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-          <CaptureButton pokemon={pokemon}/>
+          <Box>
+            <Link className=""
+                  to={`/pokemon/${pokemon.name}`}>
+              <Typography className="pokemon-name" gutterBottom variant="h5"
+                          component="div">
+                {pokemon.name}
+                {pokemon.id &&
+                <><br/><span className="pokemon-id">(#{pokemon.id})</span></>}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000
+                species, ranging across all continents except Antarctica
+              </Typography>
+            </Link>
+          </Box>
         </CardContent>
+        <CaptureButton pokemon={pokemon}/>
       </CardActionArea>
     </Card>
-  );
+  )
+    ;
 };
-
 
 export default PokemonCard;
