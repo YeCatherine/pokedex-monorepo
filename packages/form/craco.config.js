4e3cf17129ada177527c3e095a877a796e5f6dc0
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2022-08-22 19:26:56 +0200

    update craco

--- packages/form/craco.config.js
+++ packages/form/craco.config.js
@@ -3,4 +3,10 @@ const path = require('path')
 const cracoDefaultConfig = require(
   path.join(__dirname, '../components', 'craco.config.js'))
 
+cracoDefaultConfig.webpack = {
+  alias: {
+    '@': path.resolve(__dirname, 'src')
+  }
+}
+
 module.exports = cracoDefaultConfig
