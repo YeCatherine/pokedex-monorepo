38c508696471c0c38593dc6be9057054767c2f54
Katsiaryna_Yemelyanenka <Katsiaryna_Yemelyanenka@epam.com>
2022-09-18 02:28:37 +0200

    Update Packages

---
+++ packages/apps/redux-typescript/src/Layout.tsx
@@ -0,0 +1,96 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import React from 'react';
+import { GridSize } from '@material-ui/core/Grid/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'center'
+  },
+  pokemonLogo: {
+    maxWidth: '90%',
+    width: 400
+  },
+  loadingContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#fff',
+    fontSize: 24,
+    textTransform: 'uppercase'
+  },
+  loadingText: {
+    marginTop: theme.spacing(2)
+  }
+}));
+type Props = {
+  top?: React.ReactNode;
+  sidebar?: React.ReactNode;
+  children: React.ReactNode;
+  loading?: any;
+  logo?: any;
+  title?: React.ReactNode;
+};
+
+export const Layout: React.FC<Props> = (
+    {
+      children, top, sidebar, loading, logo, title
+    }) => {
+    const classes = useStyles();
+    let columnSize = 0 as GridSize;
+    let contentSize = 12 as GridSize;
+    if (sidebar !== undefined) {
+      columnSize = 3 as GridSize;
+      contentSize = 9 as GridSize;
+    }
+
+    // @ts-ignore
+    return (
+      <main className={classes.root}>
+        <Container>
+          <img src={logo} alt="" className={classes.pokemonLogo}/>
+          <Typography variant="srOnly">
+            <h1>{title}</h1>
+          </Typography>
+          {loading ? (
+            <>
+              {top}
+              <Grid container className={classes.root} spacing={4}>
+                <Grid
+                  item
+                  xs={12}
+                  sm={12}
+                  md={columnSize}
+                  lg={columnSize}
+                >
+                  <Card variant="outlined">{sidebar}</Card>
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  sm={12}
+                  md={contentSize}
+                  lg={contentSize}
+                >
+                  {children}
+                </Grid>
+              </Grid>
+            </>
+          ) : (
+            <div className={classes.loadingContainer}>
+              <CircularProgress color="inherit" size={60}/>
+              <Typography className={classes.loadingText}>Loading</Typography>
+            </div>
+          )}
+        </Container>
+      </main>
+    );
+  }
+;
+
+export default Layout;
